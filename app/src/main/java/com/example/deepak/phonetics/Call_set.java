ee0d1dc7155d409485f04dfbcccf05c319aa35b2
Deepak Pal <deepakpal.9046@gmail.com>
2017-06-02 01:14:36 +0530

    Pre Change

---
+++ app/src/main/java/com/example/deepak/phonetics/Call_set.java
@@ -0,0 +1,23 @@
+package com.example.deepak.phonetics;
+
+/**
+ * Created by deepak on 5/28/2017.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Call_set extends AppCompatActivity{
+
+        /** Called when the activity is first created. */
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.call_set);
+
+
+        }
+}
--- app/src/main/java/com/example/deepak/phonetics/MainActivity.java
+++ app/src/main/java/com/example/deepak/phonetics/MainActivity.java
@@ -5,27 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected
+void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Declare our Text Variables and assign them views from the layout
 
-       // TextView answerLabel = (TextView) findViewById(R.id.Text_box);
-        Button getAnswerButton = (Button) findViewById(R.id.LAN_BTN);
 
-        getAnswerButton.setOnClickListener(new View.OnClickListener() {
-    public void onClick(View v) {
-        // Perform action on click
-       // MainActivity.setContentView(R.layout.language_setting);
-        Intent intent = new Intent(v.getContext(),Language_STN.class);
-        startActivityForResult(intent,0);
-    }
+        Button getAnswerButton = (Button) findViewById(R.id.btn_call);
+
+
+        getAnswerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //On click function
+            public void onClick(View view) {
+                //Create the intent to start another activity
+                Intent intent = new Intent(view.getContext(), Call_set.class);
+                startActivity(intent);
+            }
         });
     }
 }
